5b14f395dc2caec187c0f834b47a938a750b8ef0
Alex Aralis <alex@gmail.com>
2015-11-25 20:47:49 -0800

    initial commit with a sudoku grid

---
+++ sudoku.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> Sudoku </title>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/components/sudoku.css.html'; ?>    
+</head>
+
+<body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/components/sudoku.html'; ?>
+
+</body>
+</html>
